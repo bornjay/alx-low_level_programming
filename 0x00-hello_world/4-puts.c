c82a9ba5c12e4b607ca163665c2517accabb09e0
bornjay <oluwayemisikolade@gmail.com>
2022-09-12 15:44:51 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
+#include<stdio.h>
 /**
  * main - Print C
  *
- * Return: Always 0( Successful)
+ * Return: Always 0 (Successful)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
