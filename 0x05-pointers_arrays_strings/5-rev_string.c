de0629ab073e70992fd9a4bdef46b81749b37065
bornjay <oluwayemisikolade@gmail.com>
2022-09-21 15:41:21 -0700

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,4 +18,21 @@ void rev_string(char *s)
 		*(s + i) = *(s + lrn - i - 1);
 		*(s + len - i - 1) = tmp;
 	}
+
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: the length of the given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
 }
