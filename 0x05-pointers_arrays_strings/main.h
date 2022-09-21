4ddc17c90add0e756a418f8a1e0043ec0119f1d8
bornjay <oluwayemisikolade@gmail.com>
2022-09-20 17:54:19 -0700

    reset

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /**
-* void prototypes()
-* int prototypes()
-*/
+ * void prototypes()
+ * int prototypes()
+ */
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -19,3 +19,4 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *str);
 
 #endif /*MAIN_H*/
+
