d58276780144d80ae7d3064d45a3424d51ca3ed9
bornjay <oluwayemisikolade@gmail.com>
2022-09-25 17:32:20 -0700

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - print putchar
- * Return : always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -13,9 +13,8 @@ int main(void)
 	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar('a');
+	_putcahr('a');
 	_putchar('r');
-	_putchar('\n');
 
 	return (0);
 }
